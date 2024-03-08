4331f1daa9d7135748aa17a2b66d5fbde89550f8
ShdwKick <4d5.gamedev@gmail.com>
2024-03-08 11:11:45 +0300

    Init starter content

---
+++ Source/ArcSnake/ArcSnakeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArcSnakeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCSNAKE_API AArcSnakeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
